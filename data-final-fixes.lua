cdccedc1c14cb7285e29a296c3bae9e3ba91f0d4
Vidar Wahlberg <canidae@exent.net>
2020-11-04 11:48:08 +0100

    Version 5.2.0 by SirTony

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,38 +1,40 @@
--- dynamically add sprites for tools (to display research ingredients)
-for _, tool in pairs( data.raw.tool ) do
-    local w = 0
-    local h = 0
-    local f = nil
-    local ok = false
+-- Dynamically add sprites for tools (to display research ingredients)
+for _, tool in pairs(data.raw.tool) do
+    -- Setup locals
+    local sprite_layers = {}
 
-    -- icon specification requires either
-    --   - icon and icon_size properties to be defined
-    --     OR
-    --   - icons (and optionally icon_size) to be defined
-
-    if tool.icon and tool.icon_size then
-        w = tool.icon_size
-        h = tool.icon_size
-        f = tool.icon
-        ok = true
-    elseif tool.icons and tool.icons[1] and tool.icons[1].icon_size then
-        w = tool.icons[1].icon_size
-        h = tool.icons[1].icon_size
-        f = tool.icons[1].icon
-        ok = true
+    -- Check for an icons definition
+    if tool.icons then
+        -- We're working with an icons definition, this may be fully defined or only partially defined, if partially defined we will fully define it for our local layer
+        for _, icon_data in pairs(tool.icons) do
+            table.insert(sprite_layers, {
+                filename = icon_data.icon,
+                size = icon_data.icon_size or tool.icon_size,
+                mipmap_count = icon_data.icon_mipmaps or tool.icon_mipmaps,
+                tint = icon_data.tint,
+                blend_mode = icon_data.blend_mode,
+                shift = icon_data.shift,
+                scale = 20 / (icon_data.icon_size or tool.icon_size),
+                flags = {"gui-icon"},
+            })
+        end
+    else
+        -- We're working with a standard icon, no icons table
+        table.insert(sprite_layers, {
+            filename = tool.icon,
+            size = tool.icon_size,
+            mipmap_count = tool.icon_mipmaps,
+            scale = 20 / tool.icon_size,
+            flags = {"gui-icon"},
+        })
     end
 
-    if ok then
-        data:extend( {
-            {
-                type     = "sprite",
-                name     = "auto_research_tool_" .. tool.name,
-                filename = f,
-                priority = "extra-high-no-scale",
-                width    = w,
-                height   = h,
-                scale    = 16.0 / math.max( w, h ),
-            }
-        } )
-    end
+    -- Build the sprite
+    data:extend({
+        {
+            type = "sprite",
+            name = "auto_research_tool_" .. tool.name,
+            layers = sprite_layers,
+        }
+    })
 end
